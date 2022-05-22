fecdc191255af9d5d168fb8f6361c8cd6257f90f
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-05-22 18:42:51 +0000

    [Add]12-3

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   get 'books' => 'books#index'
   get 'books' => 'books#new'
   get 'books/:id' => 'books#show' , as: 'book'
-  get 'books/:id/edit' => 'books#edit'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   post 'books' => 'books#create'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'book/:id' => 'books#destroy', as: 'destroy_book'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
